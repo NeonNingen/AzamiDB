72704f503c6fbc0ff37a471180a1d1ecba4f244e
NeonNingen <Zaincheema1@aol.com>
2020-05-20 19:47:29 +0100

    Quick Fixes V15

--- randomimg/__init__.py
+++ randomimg/__init__.py
@@ -1,5 +1,18 @@
+from random import randint, choice
+
 def slap():
-	import slap.py
+	slap_list = [
+	'https://media.giphy.com/media/l3vRb8wtvRtW9mOk0/giphy.gif',
+	'https://media.giphy.com/media/l1IYa5UYE8iBLWp6E/giphy.gif',
+	'https://media0.giphy.com/media/xUPGcF8xjWmt2CRuo0/giphy.gif',
+	'https://media1.giphy.com/media/JBsgmn8uGR5hm/giphy.gif'
+	]
+	return slap_list[randint(0,2)]
 
 def shoot():
-	import shoot.py
+	shoot_list = [
+	"http://static1.comicvine.com/uploads/original/11127/111275532/5288551-9830962548-latest",
+	"http://i.imgur.com/hPL5TGD.gif"
+	]
+	shoot_img = choice(shoot_list)
+	return shoot_img
