512daa04838362e5114b7130367246fb4037f5b7
NeonNingen <Zaincheema1@aol.com>
2020-05-21 13:29:26 +0100

    Quick Fix V28

--- randomimg/__init__.py
+++ randomimg/__init__.py
@@ -11,7 +11,6 @@ def slap():
 	'https://media.giphy.com/media/l3vRb8wtvRtW9mOk0/giphy.gif',
 	'https://media.giphy.com/media/l1IYa5UYE8iBLWp6E/giphy.gif',
 	'https://media0.giphy.com/media/xUPGcF8xjWmt2CRuo0/giphy.gif',
-	'https://media1.giphy.com/media/JBsgmn8uGR5hm/giphy.gif'
 	]
 	return choice(slap_list)
 
