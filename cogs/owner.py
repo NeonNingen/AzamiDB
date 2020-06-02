d45962b3bfc4a2ac61948395dee893a8226dd43b
NeonNingen <Zaincheema1@aol.com>
2020-06-02 13:39:53 +0100

    Updated Owner Cog

--- cogs/owner.py
+++ cogs/owner.py
@@ -6,6 +6,7 @@ class Owner(commands.Cog, command_attrs=dict(hidden=True)):
 
 	def __init__(self, azami):
 		self.azami = azami
+		self.hidden = True
 
 	@commands.command(name='load', description='Owner of Azami Only')
 	@commands.is_owner()
@@ -32,10 +33,31 @@ class Owner(commands.Cog, command_attrs=dict(hidden=True)):
 		print(f"The cog, {extension} has reloaded")
 		await ctx.send(f"The cog, {extension.capitalize()} has reloaded")
 
+	@commands.command(aliases=['hban'], description="You can ban anyone, even if they're not in the server")
+	@commands.has_permissions(ban_members=True)
+	async def hackban(self, ctx, user_id: int):
+		author = ctx.message.author
+		guild = author.guild
+
+		user = guild.get_member(user_id)
+		if user is not None:
+			return await ctx.invoke(self.ban, user=user)
+
+		try:
+			await self.azami.http.ban(user_id, guild.id, 0)
+			await ctx.send(f'User: <@{user_id}> has been banned')
+			await ctx.message.delete()
+		except discord.NotFound:
+			await ctx.message.delete()
+			await ctx.send(f'User: <@{user_id}> has cannot be found')
+		except discord.errors.Forbidden:
+			await ctx.message.delete()
+			await ctx.send(f'User: <@{user_id}> has not been banned due to your permissions')
+
 	@load.error
 	async def load_error(self, ctx, error):
 		if isinstance(error, NotOwner):
-			await ctx. send("You must be the owner of this bot to use this command")
+			await ctx.send("You must be the owner of this bot to use this command")
 		elif isinstance(error, commands.CommandInvokeError):
 			await ctx.send(f"That cog doesn't exist")
 		elif isinstance(error, MissingRequiredArgument):
@@ -44,7 +66,7 @@ class Owner(commands.Cog, command_attrs=dict(hidden=True)):
 	@unload.error
 	async def unload_error(self, ctx, error):
 		if isinstance(error, NotOwner):
-			await ctx. send("You must be the owner of this bot to use this command")
+			await ctx.send("You must be the owner of this bot to use this command")
 		elif isinstance(error, commands.CommandInvokeError):
 			await ctx.send("Invalid arguement, did you check if it's lower case or missing an arguement?")
 		elif isinstance(error, MissingRequiredArgument):
@@ -53,11 +75,20 @@ class Owner(commands.Cog, command_attrs=dict(hidden=True)):
 	@_reload.error
 	async def reload_error(self, ctx, error):
 		if isinstance(error, NotOwner):
-			await ctx. send("You must be the owner of this bot to use this command")
+			await ctx.send("You must be the owner of this bot to use this command")
 		elif isinstance(error, commands.CommandInvokeError):
 			await ctx.send("Invalid arguement, did you check if it's lower case or missing an arguement?")
 		elif isinstance(error, MissingRequiredArgument):
 			await ctx.send("Requires an argument")
+
+	@hackban.error
+	async def hackban_error(self, ctx, error):
+		if isinstance(error, NotOwner):
+			await ctx.send("You must be the owner of this bot to use this command")
+		elif isinstance(error, commands.MissingPermissions):
+			await ctx.send("You cannot use this command")
+		elif isinstance(error, MissingRequiredArgument):
+			await ctx.send("Enter as follows: `ab!hackban {user_id}`")
 		
 def setup(azami):
 	azami.add_cog(Owner(azami))
