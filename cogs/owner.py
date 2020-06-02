0ff3bdc0b9c0046b125ce0e43dd3b4dd23a8ecb4
NeonNingen <Zaincheema1@aol.com>
2020-06-02 14:11:09 +0100

    Two new owner commands added

--- cogs/owner.py
+++ cogs/owner.py
@@ -54,6 +54,20 @@ class Owner(commands.Cog, command_attrs=dict(hidden=True)):
 			await ctx.message.delete()
 			await ctx.send(f'User: <@{user_id}> has not been banned due to your permissions')
 
+	@commands.command(description='Owner of Azami Only', aliases=['bi', 'boti'])
+	@commands.is_owner()
+	async def botinvite(self, ctx):
+		await ctx.send("Here's the bot invite:\n" \
+		"https://discord.com/oauth2/authorize?client_id=639574438794231818&permissions=8&scope=bot")
+
+	@commands.command(description='Owner of Azami Only', 
+					  aliases=['ownerhelp', 'oh', 'ownerh'])
+	@commands.is_owner()
+	async def owner(self, ctx):
+		cog = self.azami.get_cog('Owner')
+		commands = cog.get_commands()
+		await ctx.send([c.name for c in commands])
+
 	@load.error
 	async def load_error(self, ctx, error):
 		if isinstance(error, NotOwner):
@@ -88,7 +102,25 @@ class Owner(commands.Cog, command_attrs=dict(hidden=True)):
 		elif isinstance(error, commands.MissingPermissions):
 			await ctx.send("You cannot use this command")
 		elif isinstance(error, MissingRequiredArgument):
-			await ctx.send("Enter as follows: `a!hackban {user_id}`")
+			await ctx.send("Enter as follows: `ab!hackban {user_id}`")
+
+	@botinvite.error
+	async def botinvite_error(self, ctx, error):
+		if isinstance(error, NotOwner):
+			await ctx.send("You must be the owner of this bot to use this command")
+		elif isinstance(error, commands.CommandInvokeError):
+			await ctx.send("Invalid arguement, did you check if it's lower case or missing an arguement?")
+		elif isinstance(error, MissingRequiredArgument):
+			await ctx.send("Requires an argument")
+
+	@owner.error
+	async def owner_error(self, ctx, error):
+		if isinstance(error, NotOwner):
+			await ctx.send("You must be the owner of this bot to use this command")
+		elif isinstance(error, commands.CommandInvokeError):
+			await ctx.send("Invalid arguement, did you check if it's lower case or missing an arguement?")
+		elif isinstance(error, MissingRequiredArgument):
+			await ctx.send("Requires an argument")
 		
 def setup(azami):
 	azami.add_cog(Owner(azami))
