49097e79f70b692bab61a5d966b6c8a75fb5bc06
NeonNingen <Zaincheema1@aol.com>
2020-06-03 22:39:14 +0100

    Huge DND Update. Search Functionality WebDriver Test

--- cogs/dnd.py
+++ cogs/dnd.py
@@ -1,9 +1,21 @@
-import discord, sys, time, aiohttp
+import discord, sys, time, aiohttp, json
 from discord.ext import commands
+from discord.ext.commands import MissingRequiredArgument
 from random import randint
 from asyncio import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 sys.path.insert(1, '../')
 from diceimg import diceroll
+from driver import get_driver
+from searchjson import startEquipment, levelling
+
+color_list = [discord.Color.red(), discord.Color.green(), discord.Color.blue(),
+			  discord.Color.orange(), discord.Color.purple(), discord.Color.gold(),
+			  discord.Color.blurple(), discord.Color.greyple(), discord.Color.teal(),
+			  discord.Color.dark_red(), discord.Color.dark_green(),
+			  discord.Color.light_grey(), discord.Color.dark_gold()]
 
 def return_results(limit, rolls, mod, i=0): # Future update: Embed all commands
 	if rolls == 1:
@@ -62,6 +74,8 @@ class Dnd(commands.Cog): # Work on Embed Rolls also modifier addon
 
 	def __init__(self, azami):
 		self.azami = azami
+		self.driver = get_driver()
+		self.driver.minimize_window()
 
 	@commands.command(description=f'The format must be NdN',
 					  usage='To roll any dice, any amount of times')
@@ -257,6 +271,182 @@ class Dnd(commands.Cog): # Work on Embed Rolls also modifier addon
 						   color= discord.Color.blurple())
 		await ctx.send(embed=em)
 
+	@commands.command(name="spell search",
+					  description="Search from spells across all of dnd 5e",
+					  usage="spellsearch is written together when using the command",
+					  aliases=['ss', 'spellsearch'])
+	async def spellsearch(self, ctx, *, content):
+		content = content.lower().replace(' ', '-')
+		wait = WebDriverWait(self.driver, 5)
+		self.driver.get(f"https://www.dnd5eapi.co/api/spells/{content}")
+		body = self.driver.find_element_by_xpath('/html/body/pre').text
+		x = body
+		y = json.loads(x)
+
+		valueDesc = y['desc']
+		valueDesc = '\n\n'.join(p for p in valueDesc)
+		valueHighLvl = y['higher_level']
+		valueHighLvl = '\n\n'.join(p for p in valueHighLvl)
+		valueComp = y['components']
+		valueComp = ', '.join(p for p in valueComp)
+		valueClasses = [y['classes'][x]['name'] for x in range(len(y['classes']))]
+		valueClasses = ', '.join(p for p in valueClasses)
+
+		spell_em = discord.Embed(title="So here's the spell your looking for traveller!",
+								description=f"Information on: {y['name']}",
+								color=discord.Color.blue())
+		spell_em.set_thumbnail(url=ctx.message.author.avatar_url)
+		spell_em.add_field(name="Name", value=y['name'])
+		spell_em.add_field(name="Description", value=valueDesc)
+		spell_em.add_field(name="At a higher level", value=valueHighLvl)
+		spell_em.add_field(name="Range", value=y['range'])
+		spell_em.add_field(name="Components", value=valueComp)
+		spell_em.add_field(name="Ritual", value=y['ritual'])
+		spell_em.add_field(name="Duration", value=y['duration'])
+		spell_em.add_field(name="Concentration", value=y['concentration'])
+		spell_em.add_field(name="Casting time", value=y['casting_time'])
+		spell_em.add_field(name="Level", value=y['level'])
+		
+		spell_em.add_field(name="Classes", value=valueClasses)
+		await ctx.send(embed=spell_em)
+
+	@commands.command(name="simple classes search",
+					  description="Search from classes across all of dnd 5e. Just Simpler. Using aiohttp",
+					  usage="simpleclassessearch is written together when using the command",
+					  aliases=['scs', 'simpleclassessearch'])
+	async def simpleclassessearch(self, ctx, *, content):
+		content = content.lower().replace(' ', '-')
+		wait = WebDriverWait(self.driver, 5)
+		self.driver.get(f"https://www.dnd5eapi.co/api/classes/{content}")
+		body = self.driver.find_element_by_xpath('/html/body/pre').text
+		y = json.loads(body)
+
+		# valueDesc = y['desc']
+		# valueDesc = '\n\n'.join(p for p in valueDesc)
+		# valueHighLvl = y['higher_level']
+		# valueHighLvl = '\n\n'.join(p for p in valueHighLvl)
+		# valueComp = y['components']
+		# valueComp = ', '.join(p for p in valueComp)
+		valueProfSpells = [y['proficiency_choices'][0]['from'][x]['name'] for x in range(len(y['proficiency_choices'][0]['from']))]
+		valueProfSpells = '\n'.join(p for p in valueProfSpells)
+		try:
+			valueProfItems = [y['proficiency_choices'][1]['from'][x]['name'] for x in range(len(y['proficiency_choices'][1]['from']))]
+			valueProfItems = '\n'.join(p for p in valueProfItems)
+		except:
+			valueProfItems = 'N/A'
+		valueProfGeneral = [y['proficiencies'][x]['name'] for x in range(len(y['proficiencies']))]
+		valueProfGeneral = '\n'.join(p for p in valueProfGeneral)
+		savingThrow = [y['saving_throws'][x]['name'] for x in range(len(y['saving_throws']))]
+		savingThrow = ', '.join(p for p in savingThrow)
+		valueStartEquipmentDef, valueStartEquipmentChoose, valueStartEquipmentChoiceA, valueStartEquipmentChoiceB, valueStartEquipmentChoiceC = startEquipment(self.driver, body)
+		valueLevel1, valueLevel2, valueLevel3 = levelling(self.driver, body)
+
+		url1 = await hastebin(str(valueProfSpells), None)
+		url2 = await hastebin(str(valueProfGeneral), None)
+		url3 = await hastebin(str(valueProfSpells), None)
+		url4 = await hastebin(str(valueStartEquipmentDef), None)
+		url5 = await hastebin(str(valueStartEquipmentChoiceA), None)
+		url6 = await hastebin(str(valueStartEquipmentChoiceB), None)
+		url7 = await hastebin(str(valueStartEquipmentChoiceC), None)
+		url8 = await hastebin(str(valueLevel1), None)
+		url9 = await hastebin(str(valueLevel2), None)
+		url10 = await hastebin(str(valueLevel3), None)
+		url1 = f'[Proficiencies in Skills]({url1})'
+		url2 = f'[Proficiencies in Items]({url2})'
+		url3 = f'[Proficient in Armour and Weapons]({url3})'
+		url4 = f'[Default Starting Equipment]({url4})'
+		url5 = f'[Starting Equipment Choice 1]({url5})'
+		url6 = f'[Starting Equipment Choice 2]({url6})'
+		url7 = f'[Starting Equipment Choice 3]({url7})'
+		url8 = f'[Class Levels]({url8})'
+		url9 = f'[Ability Score per Level]({url9})'
+		url10 = f'[Proficiencies per Level]({url10})'
+
+		class_em = discord.Embed(title="So here's the class Information, traveller!",
+								description=f"Information on: {y['name']}",
+								color=discord.Color.blue())
+		class_em.set_thumbnail(url=ctx.message.author.avatar_url)
+		class_em.add_field(name="Name", value=y['name'])
+		
+		class_em.add_field(name="Hit Die", value=y['hit_die'])
+		try:
+			class_em.add_field(name="(Proficient in skills) Choose up to", value=y['proficiency_choices'][1]['choose'])
+		except:
+			class_em.add_field(name="(Proficient in skills) Choose up to", value=y['proficiency_choices'][0]['choose'])
+		class_em.add_field(name="Proficiencies in Skills", value=url1)
+		class_em.add_field(name="(Proficient in Items) Choose up to", value=y['proficiency_choices'][0]['choose'])
+		class_em.add_field(name="Proficiencies in Items", value=url2)
+		class_em.add_field(name="Proficient in Armour and Weapons", value=url3)
+		class_em.add_field(name="Saving Throws", value=savingThrow)
+		if content != 'fighter':
+			class_em.add_field(name="Default Starting Equipment", value=url4)
+		class_em.add_field(name="(Starting Equipment) Choose up to: ", value=valueStartEquipmentChoose)
+		class_em.add_field(name="Starting Equipment Choice 1", value=url5)
+		class_em.add_field(name="Starting Equipment Choice 2", value=url6)
+		class_em.add_field(name="Starting Equipment Choice 3", value=url7)	
+		class_em.add_field(name="Class Levels", value=url8)
+		class_em.add_field(name="Ability Score per Level", value=url9)
+		class_em.add_field(name="Proficiencies per Level", value=url10)
+		await ctx.send(embed=class_em)
+
+	@commands.command(name="classes search",
+					  description="Search from classes across all of dnd 5e.",
+					  usage="classessearch is written together when using the command",
+					  aliases=['cs', 'classessearch'])
+	async def classessearch(self, ctx, *, content):
+		content = content.lower().replace(' ', '-')
+		wait = WebDriverWait(self.driver, 5)
+		self.driver.get(f"https://www.dnd5eapi.co/api/classes/{content}")
+		body = self.driver.find_element_by_xpath('/html/body/pre').text
+		y = json.loads(body)
+
+		# valueDesc = y['desc']
+		# valueDesc = '\n\n'.join(p for p in valueDesc)
+		# valueHighLvl = y['higher_level']
+		# valueHighLvl = '\n\n'.join(p for p in valueHighLvl)
+		# valueComp = y['components']
+		# valueComp = ', '.join(p for p in valueComp)
+		valueProfSpells = [y['proficiency_choices'][0]['from'][x]['name'] for x in range(len(y['proficiency_choices'][0]['from']))]
+		valueProfSpells = '\n'.join(p for p in valueProfSpells)
+		try:
+			valueProfItems = [y['proficiency_choices'][1]['from'][x]['name'] for x in range(len(y['proficiency_choices'][1]['from']))]
+			valueProfItems = '\n'.join(p for p in valueProfItems)
+		except:
+			valueProfItems = 'N/A'
+		valueProfGeneral = [y['proficiencies'][x]['name'] for x in range(len(y['proficiencies']))]
+		valueProfGeneral = '\n'.join(p for p in valueProfGeneral)
+		savingThrow = [y['saving_throws'][x]['name'] for x in range(len(y['saving_throws']))]
+		savingThrow = ', '.join(p for p in savingThrow)
+		valueStartEquipmentDef, valueStartEquipmentChoose, valueStartEquipmentChoiceA, valueStartEquipmentChoiceB, valueStartEquipmentChoiceC = startEquipment(self.driver, body)
+		valueLevel1, valueLevel2, valueLevel3 = levelling(self.driver, body)
+
+		class_em = discord.Embed(title="So here's the class Information, traveller!",
+								description=f"Information on: {y['name']}",
+								color=discord.Color.blue())
+		class_em.set_thumbnail(url=ctx.message.author.avatar_url)
+		class_em.add_field(name="Name", value=y['name'])
+		
+		class_em.add_field(name="Hit Die", value=y['hit_die'])
+		try:
+			class_em.add_field(name="(Proficient in skills) Choose up to", value=y['proficiency_choices'][1]['choose'])
+		except:
+			class_em.add_field(name="(Proficient in skills) Choose up to", value=y['proficiency_choices'][0]['choose'])
+		class_em.add_field(name="Proficiencies in Skills", value=valueProfSpells)
+		class_em.add_field(name="(Proficient in Items) Choose up to", value=y['proficiency_choices'][0]['choose'])
+		class_em.add_field(name="Proficiencies in Items", value=valueProfItems)
+		class_em.add_field(name="Proficient in Armour and Weapons", value=valueProfGeneral)
+		class_em.add_field(name="Saving Throws", value=savingThrow)
+		if content != 'fighter':
+			class_em.add_field(name="Default Starting Equipment", value=valueStartEquipmentDef)
+		class_em.add_field(name="(Starting Equipment) Choose up to: ", value=valueStartEquipmentChoose)
+		class_em.add_field(name="Starting Equipment Choice 1", value=valueStartEquipmentChoiceA)
+		class_em.add_field(name="Starting Equipment Choice 2", value=valueStartEquipmentChoiceB)
+		class_em.add_field(name="Starting Equipment Choice 3", value=valueStartEquipmentChoiceC)	
+		class_em.add_field(name="Class Levels", value=valueLevel1)
+		class_em.add_field(name="Ability Score per Level", value=valueLevel2)
+		class_em.add_field(name="Proficiencies per Level", value=valueLevel3)
+		await ctx.send(embed=class_em)
+
 	@commands.command(name='dnd menu', description=f'A DND Menu',
 					  aliases=['dndm', 'mdnd'],
 					  usage='dndmenu is written together when using the command')
@@ -315,6 +505,29 @@ class Dnd(commands.Cog): # Work on Embed Rolls also modifier addon
 				await msg2.delete()
 				await self.initiative(ctx)
 				break
+
+	@spellsearch.error
+	async def spellsearch_error(self, ctx, error):
+		if isinstance(error, commands.CommandInvokeError):
+			await ctx.send(f"Incorrect Spell given")
+		elif isinstance(error, MissingRequiredArgument):
+			await ctx.send("Requires an argument")
+
+	@classessearch.error
+	async def classessearch_error(self, ctx, error):
+		if isinstance(error, commands.CommandInvokeError):
+			await ctx.send(f"Incorrect Class given")
+			print(error)
+		elif isinstance(error, MissingRequiredArgument):
+			await ctx.send("Requires an argument")
+
+	@simpleclassessearch.error
+	async def simpleclassessearch_error(self, ctx, error):
+		if isinstance(error, commands.CommandInvokeError):
+			await ctx.send(f"Incorrect Class given")
+			print(error)
+		elif isinstance(error, MissingRequiredArgument):
+			await ctx.send("Requires an argument")
 
 
 def setup(azami):
---
+++ searchjson/__init__.py
@@ -0,0 +1,77 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+
+def startEquipment(driver, body):
+	y = json.loads(body)
+	startEquipmentUrl = y['starting_equipment']['url']
+	driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't') 
+	driver.get(f"https://www.dnd5eapi.co{startEquipmentUrl}")
+	body2 = driver.find_element_by_xpath('/html/body/pre').text
+	y2 = json.loads(body2)
+
+	valueStartEquipmentDef = [y2['starting_equipment'][x]['item']['name'] + " " + str(y2['starting_equipment'][0]['quantity']) for x in range(len(y2['starting_equipment']))] 
+	valueStartEquipmentDef = '\n'.join(p for p in valueStartEquipmentDef)
+	valueStartEquipmentChoose = y2['choices_to_make']
+
+	try:
+		valueStartEquipmentChoiceA1 = ["a) " + y2['choice_1'][0]['from'][x]['item']['name'] + " or " + y2['choice_1'][1]['from'][x]['item']['name'] for x in range(len(y2['choice_1'][0]['from']))]
+		valueStartEquipmentChoiceA2 = [y2['choice_1'][2]['from'][x]['item']['name'] for x in range(len(y2['choice_1'][2]['from']))]
+		valueStartEquipmentChoiceA1 = ', '.join(p for p in valueStartEquipmentChoiceA1)
+		valueStartEquipmentChoiceA2 = ', '.join(p for p in valueStartEquipmentChoiceA2)
+		valueStartEquipmentChoiceA = valueStartEquipmentChoiceA1+" or "+valueStartEquipmentChoiceA2
+	except:
+		valueStartEquipmentChoiceA1 = ["a) " + y2['choice_1'][0]['from'][x]['item']['name'] for x in range(len(y2['choice_1'][0]['from']))]
+		valueStartEquipmentChoiceA2 = [y2['choice_1'][1]['from'][x]['item']['name'] for x in range(len(y2['choice_1'][1]['from']))]
+		valueStartEquipmentChoiceA1 = ', '.join(p for p in valueStartEquipmentChoiceA1)
+		valueStartEquipmentChoiceA2 = ', '.join(p for p in valueStartEquipmentChoiceA2)
+		valueStartEquipmentChoiceA = valueStartEquipmentChoiceA1+" or "+valueStartEquipmentChoiceA2
+
+	try:
+		valueStartEquipmentChoiceB = ["b) " + y2['choice_2'][0]['from'][x]['item']['name'] + " or " + y2['choice_2'][1]['from'][x]['item']['name'] for x in range(len(y2['choice_2'][0]['from']))]
+		valueStartEquipmentChoiceB = ', '.join(p for p in valueStartEquipmentChoiceB)
+	except:
+		valueStartEquipmentChoiceB = ["b) " + y2['choice_2'][0]['from'][x]['item']['name'] for x in range(len(y2['choice_2'][0]['from']))]
+		valueStartEquipmentChoiceB = ', '.join(p for p in valueStartEquipmentChoiceB)
+
+	try:
+		valueStartEquipmentChoiceC1 = ["c) " + y2['choice_3'][0]['from'][x]['item']['name'] for x in range(len(y2['choice_3'][0]['from']))]
+		valueStartEquipmentChoiceC2 = [y2['choice_3'][1]['from'][x]['item']['name'] for x in range(len(y2['choice_3'][1]['from']))]
+		valueStartEquipmentChoiceC1 = ', '.join(p for p in valueStartEquipmentChoiceC1)
+		valueStartEquipmentChoiceC2 = ', '.join(p for p in valueStartEquipmentChoiceC2)#
+		valueStartEquipmentChoiceC = valueStartEquipmentChoiceC1+" or "+valueStartEquipmentChoiceC2
+	except:
+		valueStartEquipmentChoiceC = "N/A"
+
+	driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 'w') 
+	return valueStartEquipmentDef, valueStartEquipmentChoose ,valueStartEquipmentChoiceA, valueStartEquipmentChoiceB, valueStartEquipmentChoiceC
+
+def levelling(driver, body):
+	y = json.loads(body)
+	levellingUrl = y['class_levels']['url']
+	driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't') 
+	driver.get(f"https://www.dnd5eapi.co{levellingUrl}")
+	body2 = driver.find_element_by_xpath('/html/body/pre').text
+	y2 = json.loads(body2)
+
+	level_list = []
+	level_list2 = []
+	level_list3 = []
+	for i in range(0, 23):
+		valueLevel1 = [y2[i]['features'][x]['name'] for x in range(len(y2[i]['features']))]
+		valueLevel2 = [y2[i]['feature_choices'][x]['name'] for x in range(len(y2[i]['feature_choices']))]
+		valueLevel2 += '\n'
+		valueLevel1 = str(f"Level {y2[i]['level']} -> ") + (', ').join(p for p in valueLevel1) + " " + (', ').join(p for p in valueLevel2)
+		if 'ability_score_bonuses' in y2[i]:
+			valueLevel3 = f"Level {y2[i]['level']} -> {y2[i]['ability_score_bonuses']}"
+			level_list2.append(valueLevel3)
+		if 'prof_bonus' in y2[i]:
+			valueLevel4 = f"Level {y2[i]['level']} -> {y2[i]['prof_bonus']}"
+			level_list3.append(valueLevel4)
+		level_list.append(valueLevel1)
+
+	level_list = '\n'.join(p for p in level_list)
+	level_list2 = '\n'.join(p for p in level_list2)
+	level_list3 = '\n'.join(p for p in level_list3)
+	return level_list, level_list2, level_list3
