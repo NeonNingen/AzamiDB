ad9c1eca657ddc883895b96738615f49f60c6922
NeonNingen <Zaincheema1@aol.com>
2020-06-14 21:21:51 +0100

    Formatting fix on welcome msg

--- AzamiDX/etc/etccont/verify.py
+++ AzamiDX/etc/etccont/verify.py
@@ -12,7 +12,7 @@ Greeting from all of us aboard the starship!
 ➤ Make sure to read: {rules.mention}
 
 ➤ Come and hangout in the startship!
-⠀⠀ at the: {hub.mention}
+   at the: {hub.mention}
 
 ➤ Need support or have queries about Azami.
    Go to the 𝐂𝐡𝐨𝐦𝐩𝐰𝐨𝐫𝐤𝐬 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐆𝐚𝐥𝐚𝐱𝐲 Category
