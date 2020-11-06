38712126c26d258dc2effe0823849baf58c1f7f0
NeonNingen <zaincheema1@aol.com>
2020-11-06 20:21:33 +0000

    AGAIb nagiinab usqho

--- AzamiDX/cogs/nagoomba.py
+++ AzamiDX/cogs/nagoomba.py
@@ -12,10 +12,13 @@ class Nagoomba(commands.Cog):
 		await ctx.send(display.nagoombaimg('stare'))
 
 	async def ichireal(self, ctx):
-		await ctx.send(display.nagoombaimg('ichireal'))
+		url = display.nagoombaimg('ichireal')
+		await ctx.send(url)
 
-	async def ichifake(self, ctx):
-		await ctx.send(display.nagoombaimg('ichifake'))
+	async def cbt(self, ctx):
+		url = display.nagoombaimg('cbt')
+		await pre_embed(titl="Just got CBT'd. Didn't like it.",
+						image_url=display.nagoombaimg('cbt'))
 
 def setup(azami):
 	azami.add_cog(Nagoomba(azami))
