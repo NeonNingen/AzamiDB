978246924a532377c49074f5cd29c44c7daecfe4
NeonNingen <zaincheema1@aol.com>
2021-06-03 12:15:29 +0100

    Testing new cog and new git push in ages

---
+++ AzamiDX/cogs/rank.py
@@ -0,0 +1,14 @@
+import discord, json
+from discord.ext import commands
+
+class rank(commands.Cog):
+
+	def __init__(self, azami):
+		self.azami = azami
+
+	@commands.command(description='Hi!')
+	async def hellotest(self, ctx):
+		await edit(ctx, content=f"Hello!, {ctx.author.mention}")
+
+def setup(azami):
+	azami.add_cog(rank(azami))
